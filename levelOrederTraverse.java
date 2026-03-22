99576d33f6a7e26cdd96d387fd99358999c55ef1
kishu2917 <krishnadp2917@gmail.com>
2026-03-22 14:47:47 +0530

    files added

---
+++ levelOrederTraverse.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if(root == null){
+            return result;
+        }
+        Queue<TreeNode> qu = new LinkedList<>();
+        qu.offer(root);
+
+        while(!qu.isEmpty()){
+            int size = qu.size();
+            List<Integer> level = new ArrayList<>();
+
+            for(int i = 0;i<size;i++){
+                TreeNode cu = qu.poll();
+                level.add(cu.val);
+
+                if(cu.left != null){
+                    qu.offer(cu.left);
+                }
+                if(cu.right != null){
+                    qu.offer(cu.right);
+                }
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}
